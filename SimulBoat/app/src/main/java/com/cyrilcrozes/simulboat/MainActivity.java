96a9367d07c70e8ab23f25c5c7d4d7390a0c3e0a
Fabien BEYLY <fabienbeyly@MacBook-Pro-de-Fabien.local>
2019-02-18 16:14:29 +0100

    Ajout de la pop up d'alerte

--- SimulBoat/app/src/main/java/com/cyrilcrozes/simulboat/MainActivity.java
+++ SimulBoat/app/src/main/java/com/cyrilcrozes/simulboat/MainActivity.java
@@ -1,12 +1,20 @@
 package com.cyrilcrozes.simulboat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.cyrilcrozes.simulboat.Scenario.ListScenario;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static java.util.jar.Pack200.Packer.ERROR;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +40,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Log.d(ERROR, "Pas entrerr");
+        if(getIntent().hasExtra("Result")) {
+            Log.d(ERROR, "Entrerr");
+            String resultValue = new String(getIntent().getStringExtra("Result"));
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle("Alerte");
+            if(resultValue != "KO"){
+                builder.setMessage("Votre scénario a bien était enregistré sous le nom : " + resultValue);
+            }
+            else {
+                builder.setMessage("Erreur, votre scenario n'a pas été enregistré.");
+
+            }
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 }
